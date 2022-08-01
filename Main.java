a97c6342e9355e94839a2dcb028c158ed42869ac
Daniel Ospina <91626236+klayngo@users.noreply.github.com>
2022-08-01 14:48:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+
+       int num1 [] = {1,2,3,4,5};
+       int save [];
+       for (int i = 0; i < num1.length; i++){
+
+           num1[i];
+
+           
+       }
+        
+        
+
+    }
+
+}
